f51b9a5c62ef4055f99266bc4410d24e2a258c03
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-02-21 22:43:45 +0300

    solved delete issue

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -141,7 +141,7 @@ const PostCard = ({ post, user }) => {
 
 
   const getUser = (post) => {
-    const id = post?.userId?.id;
+    const id = post?.userId;
     const postUser = data.find(user=> user.id === id);
     return postUser;
   };
@@ -273,7 +273,7 @@ const PostCard = ({ post, user }) => {
           {post?.comments?.length} Comments
         </p>
 
-        {user?.id === post?.userId?.id && (
+        {user?.id === post?.userId && (
           <div
             className='flex gap-1 items-center text-base text-ascent-1 cursor-pointer'
             onClick={() => deletePost(post?.id)}
